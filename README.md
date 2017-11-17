e38902b025dd642ea2c04778b7c1367a9aa642f0
andrezeRogal <andre.torgal@gmail.com>
2017-11-17 10:29:51 +0100

    progress: 404 and 500 pages

---
+++ README.md
@@ -0,0 +1,65 @@
+# user stories
+
+## 404
+
+As a user that is looking for something that doesn't exist I want to see a 404 page so that I know it doesn't for sure
+
+## 500
+
+As a user that encountered an unexpected error while using the app I want to see a nice 500 error page so that I know what happened
+
+## Signup
+
+As a Team Lead I want to signup so that I can start managing my teams
+
+## Login
+
+As a Team Lead I want to login so that I can continue managing my teams
+
+## View Teams
+
+As a Team Lead I want to view all my teams so that I can manage each one of them
+
+## Add Team
+
+As a Team Lead I want to add a team so that I can manage it
+
+## View Team Members
+
+As a Team Lead I want to view all members of a team so that I can add and delete them
+
+## Add Team Member
+
+As a Team Lead I want to add a team member so that I can include them in the upcoming assignments
+
+## Delete Team Member
+
+As a Team Lead I want to delete a team member so that they no longer show up in upcoming assignment creation
+
+## View Assignments
+
+As a Team Lead I want to see all past assignments of a team so that I can share the details with the members
+
+## View Assignment (Share)
+
+As a Team Lead I want to see all the details of a past assignment so that I can share it with the members
+
+## Create Assignment
+
+As a Team Lead I want to create a new assignment on a team so that I can assign team members to groups
+
+## Select Members for New Assignment
+
+As a Team Lead I want to select which members are available for a new assignment so that only those are included in the groups
+
+## Reshuffle Assignment
+
+As a Team Lead I want to reshuffle the pairs in a assignment so that I can make better combinations
+
+---
+
+# backlog
+
+## Delete Team
+
+## Edit Team Member
---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const dotenv = require('dotenv');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+// -- setup the app
+
+dotenv.config();
+
+// - session
+
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 60000
+  }
+}));
+
+// - database
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URI, {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+  useMongoClient: true
+});
+
+// - views
+
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main');
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// -- routes
+
+app.use('/', index);
+app.use('/users', users);
+
+// -- 404 and error handler
+
+// NOTE: requires a views/not-found.ejs template
+app.use(function (req, res, next) {
+  res.status(404);
+  res.render('not-found');
+});
+
+// NOTE: requires a views/error.ejs template
+app.use(function (err, req, res, next) {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
+});
+
+module.exports = app;
